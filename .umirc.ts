bc1a653c4f6fe82a0a7b3ca95bb4e00296bc5e28
rawbin.zhang <rawbin.zhang@lunarax.io>
2022-05-09 20:45:32 +0800

    feat: layout patch route 无法加载microApp

--- .umirc.ts
+++ .umirc.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   },
   routes: [
     { path: '/', component: '@/pages/index' },
-    { path: '/app-react17', component: '@/pages/microAppTest' },
+    // { path: '/app-react17', component: '@/pages/microAppTest' },
   ],
   fastRefresh: {},
   layout: {},
--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,34 @@ import { RequestConfig } from 'umi';
 // index.js
 import microApp from '@micro-zoe/micro-app';
 
+export function patchRoutes({ routes }) {
+  const extraRoutes = [
+    {
+      name: '/app-react17',
+      url: 'http://nextjs11.micro-zoe.com/app-react17#/',
+      baseRoute: '/app-react17',
+    },
+  ].map((item: TMicroRouteConfig) => {
+    const { name, url, baseRoute } = item;
+    return {
+      path: `${baseRoute}`,
+      component: () => {
+        return (
+          <micro-app
+            name={name} // name(必传)：应用名称
+            url={url}
+            baseroute={baseRoute} // baseroute(可选)：基座应用分配给子应用的基础路由，就是上面的 `/my-page`
+          ></micro-app>
+        );
+      },
+    };
+  });
+
+  routes.push(...extraRoutes);
+
+  console.log('当前所有路由:', routes);
+}
+
 microApp.start({
   plugins: {
     global: [
